baaa582981b8fe8d40945ed9cb51197d79b668d5
Aaron Otu <aaronotu99@gmail.com>
2024-03-24 23:22:47 -0700

    About Me & My Work

---
+++ script.js
@@ -0,0 +1,18 @@
+var tablinks = document.getElementsByClassName("tab-links")
+var tabcontents = document.getElementsByClassName("tab-contents")
+
+
+function opentab(tabname){
+  //the for loop removes all active links and tabs
+  for(tablink of tablinks){
+      tablink.classList.remove("active-link");
+
+  }
+  for(tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+
+}
+  //the code below make sure if a link is selected the css funtion is applied
+  event.currentTarget.classList.add("active-link")
+  document.getElementById(tabname).classList.add("active-tab");
+}
